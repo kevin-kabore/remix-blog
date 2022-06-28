5f7f36621ca0a22ac1ed5961ec8500241c9d4945
Kevin Kabore <kevin.kabore@onepeloton.com>
2022-06-28 14:28:05 +0000

    feat(posts/admin/new): add NewPost form

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,3 +1,40 @@
+import { Form } from "@remix-run/react";
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
 export default function NewPost() {
-  return <h2>New Post</h2>;
+  return (
+    <Form method="post">
+      <p>
+        <label>
+          Post Title:{" "}
+          <input type="text" name="title" className={inputClassName} />
+        </label>
+      </p>
+      <p>
+        <label>
+          Post Slug:{" "}
+          <input type="text" name="slug" className={inputClassName} />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">Markdown:</label>
+        <br />
+        <textarea
+          id="markdown"
+          rows={20}
+          name="markdown"
+          className={`${inputClassName} font-mono`}
+        />
+      </p>
+      <p className="text-right">
+        <button
+          type="submit"
+          className="rounded bg-blue-500 py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400 disabled:bg-blue-300"
+        >
+          Create Post
+        </button>
+      </p>
+    </Form>
+  );
 }
