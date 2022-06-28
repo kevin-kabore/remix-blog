4f1e499eb6ff0ff2bb4cf9e9f0db2f76fe54d3b1
Kevin Kabore <kevin.kabore@onepeloton.com>
2022-06-28 13:46:44 +0000

    render markdown as html with marked

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,3 +1,4 @@
+import { marked } from "marked";
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
@@ -6,7 +7,7 @@ import invariant from "tiny-invariant";
 import type { Post } from "~/models/post.server";
 import { getPost } from "~/models/post.server";
 
-type LoaderData = { post: Post };
+type LoaderData = { post: Post; html: string };
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.slug, `params.slug is required`);
@@ -14,14 +15,16 @@ export const loader: LoaderFunction = async ({ params }) => {
   const post = await getPost(params.slug);
   invariant(post, `Post not found: ${params.slug}`);
 
-  return json<LoaderData>({ post });
+  const html = marked(post.markdown);
+  return json<LoaderData>({ post, html });
 };
 
 export default function PostSlug() {
-  const { post } = useLoaderData<LoaderData>();
+  const { post, html } = useLoaderData<LoaderData>();
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
